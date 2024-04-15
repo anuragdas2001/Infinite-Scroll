679628d23faa633d79c7af2fe1b290951c2bf23f
anuragdas2001 <anuragdas12921@gmail.com>
2024-04-15 11:51:22 +0530

    Change in file structure

--- Client/src/components/Table.tsx
+++ Client/src/components/Table.tsx
@@ -1,10 +1,41 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+
+
+interface Data {
+  city: number;
+  country: string;
+  timezone: number;
+}
+// const data: Data[] = [
+//   {
+//     city: 1,
+//     country: "India",
+//     timezone: 46000,
+//   },
+//   {
+//     city: 1,
+//     country: "US",
+//     timezone: 46000,
+//   },
+//   {
+//     city: 1,
+//     country: "Australia",
+//     timezone: 46000,
+//   },
+// ];
+// city,country,Timezone
+
+
 export const Table = () => {
+  const [data, setData] = useState<Data[]>([]);
   useEffect(() => {
-    axios
-      .get("https://public.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000/api/?disjunctive.cou_name_en&sort=name")
+    const Fetchdata = axios
+      .get(
+        "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20&timezone=Asia%2FKolkata"
+      )
       .then((response) => {
+        setData(response.data)
         console.log(response.data);
       })
       .catch((error) => {
@@ -12,5 +43,21 @@ export const Table = () => {
       });
   }, []);
 
-  return <></>;
+  console.log(data);
+ 
+   
+    console.log(data);
+  return (
+    <>
+     <table className="border-4 border-black">
+      <thead>
+        <tr>
+          <td>City name</td>
+          <td>Country</td>
+          <td>Timezone</td>
+        </tr>
+      </thead>
+     </table>
+    </>
+  );
 };
